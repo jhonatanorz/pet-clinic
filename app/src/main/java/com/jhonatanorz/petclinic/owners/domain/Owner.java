6200c51a572c7bfdd932ff8a4ecdca9a5477336a
Jhonatan <jhonatan0110011@gmail.com>
2022-07-20 11:26:26 -0500

    Add static web resources (images and fonts). Closes #8

--- app/src/main/java/com/jhonatanorz/petclinic/owners/domain/Owner.java
+++ app/src/main/java/com/jhonatanorz/petclinic/owners/domain/Owner.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 @Getter
 public class Owner extends Person {
 
-    private final String id;
-
     public Owner(String id, String name, String lastName) {
         super(id, name, lastName);
-        this.id = id;
     }
 }
